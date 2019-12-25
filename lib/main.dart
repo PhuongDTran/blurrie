44e752e79d359257e9eb4e6d9a6fe2f1d1bc15e3
Phuong Tran <ptran816@uw.edu>
2019-12-25 12:56:07 -0800

    faces detected, two FABs

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
   runApp(
     MaterialApp(
       theme: ThemeData.dark(),
-      home: LoadImage()
+      home: FacePage()
     )
   );
 }
--- lib/photo_picker.dart
+++ lib/photo_picker.dart
@@ -1,49 +1,113 @@
-import 'dart:async';
 import 'dart:io';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/container.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:image_picker/image_picker.dart';
 
-class LoadImage extends StatefulWidget {
+class FacePage extends StatefulWidget {
   @override
-  _LoadImage createState() => _LoadImage();
+  _FacePageState createState() => _FacePageState();
 }
 
-class _LoadImage extends State<LoadImage> {
-  File _image;
-  dynamic _pickImageError;
+class _FacePageState extends State<FacePage> {
+  File _imageFile;
+  List<Face> _faces;
+  bool isLoading = false;
+  ui.Image _image;
 
-  void _getImage(ImageSource source, {BuildContext context}) async {
+  _getImageAndDetectFaces() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (imageFile == null) {
+      return;
+    }
+    setState(() {
+      isLoading = true;
+    });
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final faceDetector = FirebaseVision.instance.faceDetector();
+    List<Face> faces = await faceDetector.processImage(image);
+    if (mounted) {
+      setState(() {
+        _imageFile = imageFile;
+        _faces = faces;
+        _loadImage(imageFile);
+      });
+    }
+  }
 
-        try {
-          _image = await ImagePicker.pickImage(
-              source: source
-              );
-          setState(() {});
-        } catch (e) {
-          _pickImageError = e;
-        }
+  _loadImage(File file) async {
+    final data = await file.readAsBytes();
+    await decodeImageFromList(data).then(
+      (value) => setState(() {
+        _image = value;
+        isLoading = false;
+      }),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Picker Example'),
-      ),
-      body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {_getImage(ImageSource.gallery, context: context);
-        },
-        tooltip: 'Pick Image',
-        child: Icon(Icons.add_a_photo),
-      ),
-    );
+        body: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : (_imageFile == null)
+                ? Center(child: Text('No image selected'))
+                : Center(
+                    child: FittedBox(
+                      child: SizedBox(
+                        width: _image.width.toDouble(),
+                        height: _image.height.toDouble(),
+                        child: CustomPaint(
+                          painter: FacePainter(_image, _faces),
+                        ),
+                      ),
+                    ),
+                  ),
+        floatingActionButton: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              FloatingActionButton(
+                  onPressed: _getImageAndDetectFaces,
+                  tooltip: 'Take a photo',
+                  child: Icon(Icons.add_a_photo)),
+              SizedBox(height: 5),
+              FloatingActionButton(
+                onPressed: _getImageAndDetectFaces,
+                tooltip: 'Choose a photo from library',
+                child: Icon(Icons.library_add),
+              )
+            ]));
+  }
+}
+
+class FacePainter extends CustomPainter {
+  final ui.Image image;
+  final List<Face> faces;
+  final List<Rect> rects = [];
+
+  FacePainter(this.image, this.faces) {
+    for (var i = 0; i < faces.length; i++) {
+      rects.add(faces[i].boundingBox);
+    }
+  }
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.red;
+
+    canvas.drawImage(image, Offset.zero, Paint());
+    for (var i = 0; i < faces.length; i++) {
+      canvas.drawRect(rects[i], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FacePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
   }
 }
